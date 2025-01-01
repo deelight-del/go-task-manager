271f33e7712fd5c288bf90e643ad367c9145184e
deelight <akingbenidavidoyin@gmail.com>
2025-01-01 16:28:16 +0100

    function completed without CLI interaction

--- task-manager.go
+++ task-manager.go
@@ -3,17 +3,16 @@ package main
 import (
   "fmt"
   "github.com/fatih/color"
+  //"os"
 )
 
-var count int = 0
+//var count int = 0
 
 type task struct {
   name, status string
 }
 
-type taskMap map[int]*task
-
-var tasks taskMap = make(taskMap)
+var tasks = make([]*task, 0)
 
 func addTask(name string) {
   //Create task struct.
@@ -22,23 +21,51 @@ func addTask(name string) {
     name,
     "pending",
   }
-  tasks[count] = &t
-  count++
+  tasks = append(tasks, &t)
+  //count++
 }
 
 func deleteTask(id int) {
-  delete(tasks, id)
+  //delete(tasks, id) 
+  if id >= len(tasks) {
+    //TODO: Throw and handle some error.
+    return
+  }
+  ret := make([]*task, 0)
+  ret = append(ret, tasks[:id]...)
+  tasks = append(ret, tasks[id+1:]...)
 }
 
 func modifyStatus(id int, newStatus string) {
+  if newStatus != "completed" && newStatus != "pending" {
+    fmt.Println("Unrecognized status")
+    return
+  }
   (tasks[id]).status = newStatus
 }
 
-func listTasks() {
+func listTasks(filter ...string) {
   c := color.New(color.FgCyan, color.Bold)
   c.Printf("%-3s ------>    %-20s --- %10s\n", "ID", "Task", "Status")
-  for k, v := range(tasks) {
-    fmt.Printf("%-3d ------>    %-20s --- %10s\n", k, v.name, v.status)
+  if len(filter) == 0 {
+    for i, v := range(tasks) {
+      fmt.Printf("%-3d ------>    %-20s --- %10s\n", i, v.name, v.status)
+    }
+  } else {
+    if filter[0] == "pending" {
+      for i, v := range(tasks) {
+        if v.status == "pending" {
+          fmt.Printf("%-3d ------>    %-20s --- %10s\n", i, v.name, v.status)
+        }
+      }
+    } else if filter[0] == "completed" {
+      for i, v := range(tasks) {
+        if v.status == "completed" {
+          fmt.Printf("%-3d ------>    %-20s --- %10s\n", i, v.name, v.status)
+        }
+      }
+    }
+    fmt.Println("filter applied.")
   }
 }
 
@@ -47,8 +74,9 @@ func main() {
 
   addTask("Washing Clothes")
   addTask("Ironing clothes")
+  //addTask("Writing Codes.")
+  modifyStatus(0, "completed")
+  deleteTask(1)
   addTask("Writing Codes.")
-  modifyStatus(0, "done")
-  //deleteTask(1)
   listTasks()
 }
